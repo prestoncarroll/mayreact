2fee397c37d0ebaabe21073079571278f56cd4f3
Preston Carroll <plutusinc1@yahoo.com>
2020-06-01 20:57:20 -0500

    working with functions and js objects

--- mayreact/src/index.js
+++ mayreact/src/index.js
@@ -5,24 +5,38 @@ import ReactDOM from 'react-dom';
 
 //create a react component
 const App = () => {
+
     return <div>hi there! </div>;
 };
 
-// const Nop = () => {
-//     return <div> testing root successful</div>;
-// };
+// function getButtonText() {
+//     return "click on me";
+// }
 
 const Sic = () => {
+    const buttonText = {
+        text: 'click me!!'
+    }
+    const style = { backgroundColor: 'blue', color: 'white' }
+
+    const jsFor = 1 + 1;
+
     return (
         <div>
 
-            <label class="label" for="name"> enter name:</label>
+            <label className="label" htmlFor="name"> enter name: </label>
             <input id="name" type="text" />
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>submit</button>
+            <button style={style}
+            >{buttonText.text} {jsFor}</button>
 
-        </div>
+
+
+        </div >
     );
 };
+
+
+
 //take the react component and show it on the screen
 ReactDOM.render(
     <App />,
